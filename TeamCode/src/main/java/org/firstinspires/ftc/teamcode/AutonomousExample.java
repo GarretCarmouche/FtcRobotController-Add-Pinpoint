10c588225082059f2b62118520f9c19d223fb9ab
GarretCarmouche <carmouchegarret@gmail.com>
2025-12-16 17:28:13 -0600

    Example commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousExample.java
@@ -19,6 +19,7 @@ public class AutonomousExample extends LinearOpMode {
     public void runOpMode() {
         odo = hardwareMap.get(GoBildaPinpointDriver.class,"odo");
 
+        //HERE WE GO
 
         odo.setOffsets(-84.0, -168.0, DistanceUnit.MM);
         odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
